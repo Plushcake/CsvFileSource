b351af16d5cb8ca997cf2723a77c7c28e4eeaae9
Plushcake <Nikkolo.Makeawelli@gmail.com>
2022-05-13 23:35:57 +0300

    Added src folder

---
+++ src/test/java/BonusServiceTest.java
@@ -0,0 +1,20 @@
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BonusServiceTest {
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/data.csv")
+
+    public void mainCalculateTest (String test, long amount, boolean registered, long expected) {
+        BonusService service = new BonusService();
+
+        long actual = service.calculate(amount, registered);
+
+        assertEquals(actual, expected);
+    }
+
+}
